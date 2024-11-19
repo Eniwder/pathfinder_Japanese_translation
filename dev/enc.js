e773208e44cf2e69560a0ce545b8e10df6f47f0d
Eniwder <akabudou0120@yahoo.co.jp>
2024-11-19 17:25:44 +0900

    fix etc

--- dev/enc.js
+++ dev/enc.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
-const engOrg = fs.readFileSync('./public/enGB_org.json');
+const engOrg = fs.readFileSync('./lang/enGB.json');
 // console.log(engOrg);
 
 const eng = xor2(engOrg, Buffer.from(process.env.DEC_KEY, 'hex'));
 
 console.log(eng);
 
-fs.writeFileSync('./public/enGB.json', eng);
+fs.writeFileSync('../public/enGB.json', eng);
 
 function xor2(hex1, hex2) {
   const buf1 = Buffer.from(hex1, 'hex');
